2f2068d38e636186ee5be501919408b22c3e6cfc
원종민 <won.dev@mobidoo.co.kr>
2025-07-04 23:41:47 +0900

    Revert "testContainer 도입"
    This reverts commit 6f33fbef0878ba2396f70a7cb69d3cc2ddab3eba.

--- src/test/java/com/example/batch/PlayerUserlogBatchTest.java
+++ src/test/java/com/example/batch/PlayerUserlogBatchTest.java
@@ -1,24 +1,38 @@
 package com.example.batch;
 
+import com.example.batch.entity.PlayerUserLog;
 import com.example.batch.repository.PlayerUserLogRepository;
 import com.example.batch.service.TestDataGeneratorService;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.test.JobLauncherTestUtils;
-import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 @Import(TestConfig.class)
-@SpringBatchTest
 @Slf4j
 public class PlayerUserlogBatchTest {
     @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job userJob;
+
+    @Autowired
     private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private JobRepository jobRepository;
 
     @Autowired
     private TestDataGeneratorService testDataGeneratorService;
@@ -58,4 +72,17 @@ public class PlayerUserlogBatchTest {
 
         //then
     }
+
+    @Bean
+    public ItemReader<PlayerUserLog> playerUserLogReader(){
+        return new ItemReader<PlayerUserLog>() {
+            @Override
+            public PlayerUserLog read() {
+                // 실제 구현은 데이터베이스에서 PlayerUserLog를 읽어오는 로직이 필요합니다.
+                // 여기서는 단순히 null을 반환하여 더 이상 읽을 데이터가 없음을 나타냅니다.
+                return null;
+            }
+        };
+    }
+
 }
